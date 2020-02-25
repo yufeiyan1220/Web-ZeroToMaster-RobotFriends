d7eb1f2c84da47f8047291409e411362644a8231
yufeiyan1220 <yufeiyan1220@live.com>
2020-02-24 20:12:49 -0600

    Updates

---
+++ precache-manifest.64e819408769936618f782139672d7bb.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b1e5b587b195bf4a69354ad824b19e3d",
+    "url": "/Web-ZeroToMaster-RobotFriends/index.html"
+  },
+  {
+    "revision": "8ccfe9ac205ee61cf2eb",
+    "url": "/Web-ZeroToMaster-RobotFriends/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "f6d81821dea08dbfec66",
+    "url": "/Web-ZeroToMaster-RobotFriends/static/css/main.0c9aae8f.chunk.css"
+  },
+  {
+    "revision": "8ccfe9ac205ee61cf2eb",
+    "url": "/Web-ZeroToMaster-RobotFriends/static/js/2.4196f117.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/Web-ZeroToMaster-RobotFriends/static/js/2.4196f117.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f6d81821dea08dbfec66",
+    "url": "/Web-ZeroToMaster-RobotFriends/static/js/main.74b22704.chunk.js"
+  },
+  {
+    "revision": "ab1fb9cea36291e6f757",
+    "url": "/Web-ZeroToMaster-RobotFriends/static/js/runtime-main.cda8e0a8.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/Web-ZeroToMaster-RobotFriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
