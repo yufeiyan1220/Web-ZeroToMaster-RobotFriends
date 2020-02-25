a335cca3b4dc4ce19037ef04e0a8f136658a0e19
yufeiyan1220 <yufeiyan1220@live.com>
2020-02-24 20:02:12 -0600

    Add errorboundry

--- src/components/CardList.js
+++ src/components/CardList.js
@@ -1,27 +1,19 @@
-import React, {Component} from 'react';
+import React from 'react';
 import Card from './Card';
 
 const robohash = "https://robohash.org/";
 const options = "?size=200x200";
 
-class CardList extends Component {
-  constructor(props) {
-    super(props);
-    // console.log("card_components", props.card_components);
-    this.robots = props.card_components;
-  }
-  render() {
 
-    let card_list = this.robots.map((robot)=>{return new Card(robot, robohash, options).render()});
+const CardList = ({card_components})=>{
+  let card_list = card_components.map((robot)=>{return new Card(robot, robohash, options).render()});
 
-    return (
-      <div>
-        {card_list}
-      </div>
-    );
-  }
+  return (
+    <div>
+      {card_list}
+    </div>
+  );
 }
-
 // const CardList = (props) => {
 //   let card_list = props.card_components.map((robot)=>{return new Card(robot, robohash, options).render()});
 //
--- src/containers/App.js
+++ src/containers/App.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import CardList from '../components/CardList';
 import SearchBox from '../components/SearchBox';
 import Scroll from '../components/Scroll';
+import ErrorBoundry from '../components/ErrorBoundry';
 import './App.css';
 
 class App extends Component {
@@ -36,10 +37,6 @@ class App extends Component {
       return robot.name.toLowerCase().includes(this.state.searchfield.toLowerCase());
     });
 
-    let cl = new CardList({
-      card_components: filteredRobots
-    });
-
     if(this.state.robots.length === 0) {
       return (
         <div className = "tc">
@@ -53,7 +50,9 @@ class App extends Component {
           <h1 className = 'f1'> Robot Friends </h1>
           <SearchBox searchChange = {this.onSearch}/>
           <Scroll>
-            {cl.render()}
+            <ErrorBoundry>
+              <CardList card_components={filteredRobots}/>
+            </ErrorBoundry>
           </Scroll>
         </div>
       );
